e33b5061aad06f6e9b0ac34d968eff2c40355415
Lucas <lucasgoncalves08@live.com>
2024-01-20 14:57:50 -0300

    finishing basic search input styles for mobile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './_components/navbar/navbar.component';
+import { SearchCountryComponent } from './_components/search-country/search-country.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    SearchCountryComponent
   ],
   imports: [
     BrowserModule,
