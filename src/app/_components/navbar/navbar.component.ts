46c9b39f87d5150bf13a0d1b3d121a2d43389b96
Lucas <lucasgoncalves08@live.com>
2024-01-20 14:34:21 -0300

    adding dark mode functionality

--- src/app/_components/navbar/navbar.component.ts
+++ src/app/_components/navbar/navbar.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
 
+  darkMode = false;
+
+  ngOnInit(): void {
+    this.gettingThemeFromLocalStorage();
+  }
+
+  darkModeToggle(){
+    this.darkMode = !this.darkMode;
+    this.setTheme();
+  }
+
+  private gettingThemeFromLocalStorage(){
+
+    if(localStorage.getItem('data-theme')) {
+      localStorage.getItem('data-theme') === 'dark-theme' ? this.darkMode = true : this.darkMode = false;
+      this.setTheme();
+    }
+  }
+
+  private setTheme(){
+    if(this.darkMode) {
+      document.documentElement.setAttribute('data-theme', 'dark-theme');
+      localStorage.setItem('data-theme', 'dark-theme');
+    } else {
+      document.documentElement.setAttribute('data-theme', 'light-theme');
+      localStorage.setItem('data-theme', 'light-theme');
+    }
+  }
 }
